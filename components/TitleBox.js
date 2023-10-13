bead282dbc51d5e47c441ef7ef5c0dc0c809e7e9
Duncan Grimes <duncan.p.grimes@vanderbilt.edu>
2023-10-13 10:22:42 -0500

    bigger titles

--- components/TitleBox.js
+++ components/TitleBox.js
@@ -1,5 +1,5 @@
 import {Paper, Typography} from "@mui/material";
-
+import '../styles/styles.css';
 
 export default function TitleBox({title}){
     return (<Paper className="smallPaper" elevation={24} sx={{backgroundColor: 'primary.dark'}}>
