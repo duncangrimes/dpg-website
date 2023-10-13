0cb6edf171cdd8b2680696d43cb0addc66772741
Duncan Grimes <duncan.p.grimes@vanderbilt.edu>
2023-10-13 18:09:06 -0500

    smaller titles

--- components/PageContent.js
+++ components/PageContent.js
@@ -2,7 +2,8 @@ import {Box} from "@mui/material";
 import TitleBox from "./TitleBox";
 
 export default function PageContent({height, title, children}){
-    return (<Box sx={{height: height, display: 'flex', alignItems: 'center', justifyContent: 'start', flexDirection: 'column', paddingBottom: '10vh'}}>
+    return (<Box sx={{height: height, display: 'flex', alignItems: 'center', justifyContent: 'start', flexDirection: 'column',
+        paddingBottom: '10vh', width: '100%'}}>
         <TitleBox title={title}/>
         {children}
     </Box>);}
