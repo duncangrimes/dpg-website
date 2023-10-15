f3c043eb6df892eb7aedd5a82f7b0740051b904c
Duncan Grimes <duncan.p.grimes@vanderbilt.edu>
2023-10-15 14:08:48 -0500

    removed padding

--- components/Footer.js
+++ components/Footer.js
@@ -6,23 +6,24 @@ import ContactInfo from "./ContactInfo";
 export default function Footer() {
     return <AppBar className="appBar" position="static" sx={{height: {xs:'30vh', md:'20vh'}, bottom: 0}}>
             <Toolbar sx={{justifyContent: 'center'}}>
-                <Box sx={{display: 'flex', flexDirection: {xs: 'column-reverse', md: 'row'}, justifyContent: 'flex-start', alignItems: 'center'}}>
+                <Box sx={{display: 'flex', paddingTop: '2vh', flexDirection: {xs: 'column-reverse', md: 'row'},
+                                justifyContent: {xs: 'flex-start', md: 'center'}, alignItems: {xs: 'center', md: 'flex-start'}}}>
                     <ContactInfo/>
-                    <Stack sx={{padding: '1vh'}} direction="row" spacing="7vh">
-                    <Button variant="text" color="inherit" startIcon={<LinkedInIcon style={{color: "#7290a1", fontSize: "4vh"}}/>}
-                            href="https://www.linkedin.com/in/duncan-grimes-a84641230/">
-                        <Typography className="buttonText" sx={{fontSize: {xs: 0, md: '3vh'}}}>LinkedIn</Typography>
-                    </Button>
-                    <Button variant="text"  startIcon={<GitHubIcon style={{color: "#7290a1", fontSize: "4vh"}}/>}
-                            href="https://github.com/duncangrimes">
-                        <Typography className="buttonText" sx={{fontSize: {xs: 0, md: '3vh'}}}>GitHub</Typography>
-                    </Button>
-                        <Button variant="text" startIcon ={
-                            <Avatar style={{height: '5vh', width: '5vh'}} src ="fusion-logo.png"  alt="fusion-logo"/>}
-                        href = "https://www.vanderbiltfusion.org/">
-                            <Typography className="fusionButtonText" sx={{fontSize: {xs: 0, md: '3vh'}}}>Fusion Project</Typography>
+                    <Stack direction="row" spacing="7vh">
+                        <Button variant="text" color="inherit" startIcon={<LinkedInIcon style={{color: "#7290a1", fontSize: "4vh"}}/>}
+                                href="https://www.linkedin.com/in/duncan-grimes-a84641230/">
+                            <Typography className="buttonText" sx={{fontSize: {xs: 0, md: '3vh'}, width: {xs: 0, md: 'auto'}}}>LinkedIn</Typography>
                         </Button>
-                </Stack>
+                        <Button variant="text"  startIcon={<GitHubIcon style={{color: "#7290a1", fontSize: "4vh"}}/>}
+                                href="https://github.com/duncangrimes">
+                            <Typography className="buttonText" sx={{fontSize: {xs: 0, md: '3vh'}, width: {xs: 0, md: 'auto'}}}>GitHub</Typography>
+                        </Button>
+                            <Button variant="text" startIcon ={
+                                <Avatar style={{height: '5vh', width: '5vh'}} src ="fusion-logo.png"  alt="fusion-logo"/>}
+                            href = "https://www.vanderbiltfusion.org/">
+                                <Typography className="fusionButtonText" sx={{fontSize: {xs: 0, md: '3vh'}, width: {xs: 0, md: 'auto'}}}>Fusion Project</Typography>
+                            </Button>
+                    </Stack>
                 </Box>
             </Toolbar>
         </AppBar>
--- pages/index.js
+++ pages/index.js
@@ -5,24 +5,14 @@ import HomePageContent from "../components/HomePageContent";
 export default function HomePage() {
     return <HomePageContent title='Welcome to my Website'>
         <BigPaper width='60%' color={'primary.dark'}>
-            <Typography className={'text'} sx={{color: 'primary.light', fontFamily: 'onest'}} >
+            <Typography className={'text'} sx={{color: 'primary.light', fontFamily: 'onest', display: 'inline-block'}} >
                 Hello, and thanks for stopping by my website! <br/>
                 I am a junior at Vanderbilt University studying <br/>
-            </Typography>
-            <Box sx={{display: 'flex'}}>
-                <Typography className={'text'} fontWeight={900} color={'#b52905'}>
-                        Applied Mathematics
-                </Typography>
-                <Typography className={'text'} sx={{color: 'primary.light', marginLeft: '1vh', marginRight:'1vh'}}>{"and"}</Typography>
-                <Typography className={'text'} fontWeight={900} color={'#b52905'}>
-                    Computer Science.
-                </Typography>
-            </Box>
-            <Typography className={'text'} sx={{color: 'primary.light'}}>
+                <span style={{color: '#b52905'}}> Applied Mathematics </span>
+                and <span style={{color: '#b52905'}}>Computer Science</span>. <br/>
+
                 Please enjoy exploring this site and learning a bit about me, my projects and passions.
-            </Typography>
             <br/><br/>
-            <Typography className={'text'} sx={{color: 'primary.main', alignSelf: 'start'}}>
                 - Duncan Paul Grimes
             <
             /Typography>
