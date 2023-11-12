d5f7321d56679fa71f002cbaff41f40dcbd85e84
Duncan Grimes <duncan.p.grimes@vanderbilt.edu>
2023-11-12 16:25:27 -0600

    altered homepage text

--- components/Footer.js
+++ components/Footer.js
@@ -11,16 +11,16 @@ export default function Footer() {
                     <ContactInfo/>
                     <Stack direction="row" spacing="7vh">
                         <Button variant="text" color="inherit" startIcon={<LinkedInIcon style={{color: "#7290a1", fontSize: "4vh"}}/>}
-                                href="https://www.linkedin.com/in/duncan-grimes-a84641230/">
+                                href="https://www.linkedin.com/in/duncan-grimes-a84641230/" target="_blank" rel="noopener noreferrer">
                             <Typography className="buttonText" sx={{fontSize: {xs: 0, md: '3vh'}}}>LinkedIn</Typography>
                         </Button>
                         <Button variant="text"  startIcon={<GitHubIcon style={{color: "#7290a1", fontSize: "4vh"}}/>}
-                                href="https://github.com/duncangrimes">
+                                href="https://github.com/duncangrimes" target="_blank" rel="noopener noreferrer">
                             <Typography className="buttonText" sx={{fontSize: {xs: 0, md: '3vh'}}}>GitHub</Typography>
                         </Button>
                             <Button variant="text" startIcon ={
                                 <Avatar style={{height: '5vh', width: '5vh'}} src ="fusion-logo.png"  alt="fusion-logo"/>}
-                            href = "https://www.vanderbiltfusion.org/">
+                            href = "https://www.vanderbiltfusion.org/" target="_blank" rel="noopener noreferrer">
                                 <Typography className="fusionButtonText" sx={{fontSize: {xs: 0, md: '3vh'}}}>Fusion Project</Typography>
                             </Button>
                     </Stack>
--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,17 @@ export default function HomePage() {
     return <HomePageContent title='Welcome'>
         <BigPaper width='60%' color={'primary.dark'}>
             <Typography className={'text'} sx={{color: 'primary.light', fontFamily: 'onest', display: 'inline-block'}} >
-                Hello, and thanks for stopping by my website! <br/>
+                Hello, and thanks for stopping by my website! <br/> <br/>
+
                 I am a junior at Vanderbilt University studying <br/>
                 <span style={{color: '#b52905'}}> Applied Mathematics </span>
-                and <span style={{color: '#b52905'}}>Computer Science</span>. <br/>
+                and <span style={{color: '#b52905'}}>Computer Science</span>. <br/> <br/>
 
-                Please enjoy exploring this site and learning a bit about me, my projects and passions.
+                This site is deployed with <span className={'importantHomePageText'}>Vercel</span> and <span className={'importantHomePageText'}>Next.js</span> using
+                components from <span className={'importantHomePageText'}>MaterialUI</span>, and its repository can be
+                found on my <a className={'importantHomePageText'} href="https://github.com/duncangrimes/dpg-website"target="_blank" rel="noopener noreferrer"> GitHub</a>. <br/> <br/>
+
+                Please enjoy exploring my website and learning a bit about me, my projects and passions.
             <br/><br/>
                 - Duncan Paul Grimes
             </Typography>
